ee31475fd89a8f2c0fca43924504ee463acb5347
Popa Teodor <teodor.popa96@gmail.com>
2024-04-09 21:08:23 +0300

    task 1

--- task1/src/BasicComponent.jsx
+++ task1/src/BasicComponent.jsx
@@ -1,7 +1,7 @@
 const BasicComponent = () => {
   return (
     <div>
-      <p>This is a basic React component!</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A, in quaerat numquam blanditiis maiores sit dolores harum asperiores delectus, vel sunt ex expedita molestias esse quibusdam accusantium tempora dicta doloremque.</p>
     </div>
   );
 };
