830bc2ef3bb149815305e51edb7846fcc62870b1
Popa Teodor <teodor.popa96@gmail.com>
2024-04-11 16:00:00 +0300

    i

--- task11/src/BasicComponent.jsx
+++
@@ -1,9 +0,0 @@
-const BasicComponent = () => {
-  return (
-    <div>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A, in quaerat numquam blanditiis maiores sit dolores harum asperiores delectus, vel sunt ex expedita molestias esse quibusdam accusantium tempora dicta doloremque.</p>
-    </div>
-  );
-};
-
-export default BasicComponent;
